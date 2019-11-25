12f6ea3bafbe0a947f4e81e33de167077c48b754
Viet Duc Nguyen <duc@client-141-23-179-243.wlan.tu-berlin.de>
2019-11-25 15:24:30 +0100

    Prettify code

--- adm-i/program-exercise-01/adm1.py
+++ adm-i/program-exercise-01/adm1.py
@@ -1,21 +1,19 @@
 def read(path):
-    print("open file")
+    print("Open file...")
     file = open(path, "r")
     A = parse(file)
     return A
 
 def parse(file):
-    # kommentare rausnehmen 
+    # kommentare rausnehmen
     file = [s.strip() for s in file if len(s.strip())> 0 and s.strip()[0] != "#" ]
-    print("strip file")
+    print("Strip comments from file...")
     if file == []:
         return []
     isPolyhedron = file[0] =="A"
-    print("polyhedron", isPolyhedron)
     if isPolyhedron:
         file = file[1:]
         bIndex = file.index("b")
-        print("index von b", bIndex)
     A = []
     if not isPolyhedron:
         for i in range(len(file)):
@@ -26,16 +24,20 @@ def parse(file):
         b = file[bIndex +1].split()
         for i in range(len(A)):
             A[i ].append(float(b[i]))
-    #Testfunktion, Überprüft die Dimension von A 
+    #Testfunktion, Überprüft die Dimension von A
     a = len(A[0])
     for i in range(1, len(A)):
         if len(A[i]) != a:
             print("Dimension of A at row {} is {} but should be {}".format(i, len(A[i]), a))
+    print("Matrix reads:")
+    print("\n--- MATRIX --------------------")
     printmatrix(A)
+    print("---------------------------------\n")
     return A
 
 def write(A, output_file):
-    f = open(output_file, "a")
+    print("Writing file to {}...".format(output_file))
+    f = open(output_file, "w")
     s= "A\n"
     w = ""
     for i in range(len(A)):
@@ -43,6 +45,7 @@ def write(A, output_file):
         w = w + " "+ str(A[i][-1])
     c = s + "b\n"+w.strip()+"\n"
     f.write(c)
+    print("Writing done!")
 
 #----------------------
 # Matrix Operations
@@ -52,20 +55,20 @@ def printmatrix(B):
     for i in B:
         print(i)
 
-def multiply(s, B): 
+def multiply(s, B):
     C = []
-    for i in B: 
+    for i in B:
         C.append( s * i)
-    return C 
+    return C
 
 def add(B,C):
-    if len(B) != len(C):
-        print("Länge der Vektoren stimmt nicht", len(C), len(B))
+    assert len(B) == len(C), "Länge der Vektoren stimmt nicht. dim(C)={}, dim(B)={}".format(len(C), len(B))
     D = []
     for i in range(len(B)):
         D.append(B[i] +C[i])
     return D
-def transpose(A): 
+
+def transpose(A):
     n = len(A)
     m = len(A[0])
     B = []
@@ -73,7 +76,7 @@ def transpose(A):
         B.append([])
         for j in range(0,n):
             B[i].append(A[j][i])
-    return B 
+    return B
 
 def transpose_sparse(M):
     return ([(i[0], i[2], i[1]) for i in M[0]], (M[1][1], M[1][0]))
@@ -84,7 +87,6 @@ def matrix_vector(C, b):
     Mshape = C[1]
     if M ==[]:
         return []
-    print("b", len(b), Mshape)
     assert len(b) == Mshape[1]
     c = [0]* Mshape[0]
     for i in M:
@@ -101,7 +103,7 @@ def is_greater(a,b):
     for i in range(len(a)):
         if a[i] < b[i]:
             return False
-    return True 
+    return True
 
 
 #----------------------
@@ -109,26 +111,26 @@ def is_greater(a,b):
 #----------------------
 
 def dimension_reduction(C):
-    print("Beginning dimension reduction...")
+    print("\tBeginning dimension reduction on A...")
     import copy
     if C == []:
         return C
-    if len(C[0]) < 2:
-        assert False , "EIFOK 42, spaltendimension von A ist zu klein {}".format(C)
+    assert len(C[0]) >= 2 , "EIFOK 42, spaltendimension von A ist zu klein {}".format(C)
     if len(C[0]) == 2:
         return C
     B = []
     A = copy.deepcopy(C)
 
-    print("Normalize last column of A...")
+    print("\t\tNormalize last column of A...")
     for i in range(len(A)):
         pivot = A[i][-2]
         if pivot != 0:
             A[i] = multiply(1.0/abs(pivot), A[i])
     M = []
-    row = 0 
+    row = 0
     cols = len(A)
-    print("Linear combination of rows whose last column have different signs...")
+    print("\t\tRemove last column...")
+    # Linear combination of rows whose last column have different signs
     for i in range(len(A)):
         pivot1 = A[i][-2]
         if pivot1 == 0:
@@ -150,62 +152,34 @@ def dimension_reduction(C):
                         row = row +1
     #if B == []:
         #B = [[0,0]]
+    print("\tDimension reduction done!")
     return B, (M, (row, cols))
 
 
 
 def projection(input_file, k ,output_file):
-    #reads input file 
+    k = int(k)
+    #reads input file
     A = read(input_file)
     A = __proj(A, k)
-    print("writing...")
     #write A in output_file
     write(A, output_file)
 
-def __proj(A, k): 
+def __proj(A, k):
     # empty matrix check
-    if len(A)== 0: 
+    if len(A)== 0:
         return A
     n = len(A[0])-1
+    print("Start projecting on dimension {}...".format(k))
     #apply dimension reduction n-k times
     for i in range(0, n-k):
+        print("\tProjection on dimension={}".format(n-i-1))
         A, _ = dimension_reduction(A)
-        print("\nProjection on dimension={}\n".format(n-i-1))
+        print("-------- MATRIX -------------------")
         printmatrix(A)
-        print("\n\n")
-    return A 
-
-def test():
-    file = open("project_instances.dat", "r")
-    setup = []
-    instance = None # {"k": , "data": ""}
-    content = ""
-    for line in file:
-        if len(line) == 0:
-            continue 
-        if line[0] == "k":
-            if instance != None:
-                instance["data"] = content
-                setup.append(instance)
-                content = ""
-            kValue = int(line.strip()[-1])
-            instance = {"k": kValue, "data": None}
-        else:
-            content += line
-
-    k = []
-    for i in range(len(setup)):
-        f = open("test/test{}.dat".format(i),"w")
-        content = setup[i]["data"]
-        f.write(content)
-        f.close()
-
-        k.append(setup[i]["k"])
-
-    for i in range(len(k)): 
-        projection("test/test{}.dat".format(i), k[i], "test/solution.dat")
-    
-    print("done")
+        print("-----------------------------------")
+        print("\n")
+    return A
 
 def image(input_polyhedron, input_matrix, output_file):
     A = read(input_polyhedron)
@@ -220,7 +194,6 @@ def __image(A,M):
         return None
     n = len(M[0])
     k = len(M)
-    print(k , "die länge von M ")
     # kxk Einheitsmatrix
     I1= []
     I2 = []
@@ -244,8 +217,6 @@ def __image(A,M):
         Q3.append([0]*k+ A[i])
 
     Q = Q1 +Q2 + Q3
-    #print("Q")
-    #printmatrix(Q)
     Qk = __proj(Q, k)
     return Qk
 
@@ -259,7 +230,7 @@ def H_representation(input_file, output_file):
     for i in range(0,k):
         I.append([])
         for j in range(0,k+1):
-            if i == j : 
+            if i == j :
                 I[i].append(1)
             else:
                 I[i].append(0)
@@ -270,49 +241,49 @@ def H_representation(input_file, output_file):
     write(D, output_file)
 
 def base_case(P):
-    import math 
-    #check if P is empty 
+    import math
+    #check if P is empty
     if len(P) ==0:
         return True, [42]
-    # check for impossible constrain 0 > b, b positive 
+    # check for impossible constrain 0 > b, b positive
     for i in range(len(P)):
         if P[i][0] ==0 and P[i][1] > 0:
             y = [0]* len(P)
             y[i] = 1
-            assert abs(inner_product(y, transpose(P)[0])) < 10**(-12) 
+            assert abs(inner_product(y, transpose(P)[0])) < 10**(-12)
             assert inner_product(y, transpose(P)[1]) > 0
             return False, y
-    lowerbound = - math.inf 
-    upperbound = math.inf 
-    lbIndex = None 
-    ubIndex = None 
-    # calculate lower and upperbound for x 
-    for i in range(len(P)): 
-        # improve lowerbound 
+    lowerbound = - math.inf
+    upperbound = math.inf
+    lbIndex = None
+    ubIndex = None
+    # calculate lower and upperbound for x
+    for i in range(len(P)):
+        # improve lowerbound
         if P[i][0] > 0 and P[i][1] / P[i][0] > lowerbound:
-            lowerbound =   P[i][1] / P[i][0] 
-            lbIndex = i 
-        # correct upperbound 
-        if P[i][0] < 0 and P[i][1] / P[i][0] < upperbound: 
+            lowerbound =   P[i][1] / P[i][0]
+            lbIndex = i
+        # correct upperbound
+        if P[i][0] < 0 and P[i][1] / P[i][0] < upperbound:
             upperbound = P[i][1] / P[i][0]
-            ubIndex = i 
-    # construct y if lowerbound is greater than upperbound 
-    if lowerbound > upperbound : 
+            ubIndex = i
+    # construct y if lowerbound is greater than upperbound
+    if lowerbound > upperbound :
         y = [0] *len(P)
         y[lbIndex] = 1
         y[ubIndex] = P[lbIndex][0] / (- P[ubIndex][0])
-        assert abs(inner_product(y, transpose(P)[0])) < 10**(-12) 
+        assert abs(inner_product(y, transpose(P)[0])) < 10**(-12)
         assert inner_product(y, transpose(P)[1]) > 0
         return False, y
     # return feasible x_1 if lowerbound is smaller than upperbound
-    if lowerbound <= upperbound: 
-        x = lowerbound if lowerbound != -math.inf else upperbound 
+    if lowerbound <= upperbound:
+        x = lowerbound if lowerbound != -math.inf else upperbound
         if lowerbound == -math.inf and upperbound == math.inf:
             x = 42
         assert is_greater(multiply(x, transpose(P)[0]), transpose(P)[1])
         return True, [x]
 
-def compute_x_or_y(input_file): 
+def compute_x_or_y(input_file):
     P = read(input_file)
     n = len(P[0])-1
     History = [(P, None)]
@@ -322,40 +293,40 @@ def compute_x_or_y(input_file):
         History.append((B,M))
     NotEmpty, z = base_case(History[-1][0])
     History = History[::-1]
-    if not NotEmpty: 
+    if not NotEmpty:
         for Q,M in History[:-1]:
             B = transpose_sparse(M)
             z = matrix_vector( B, z)
             #test3(Q, z)
         test3(P,z)
-        return False, z 
-    if NotEmpty : 
-        for Q, _ in History[1:]: 
+        return False, z
+    if NotEmpty :
+        for Q, _ in History[1:]:
             z = step(Q, z)
             #test4(Q,z)
-        return True, z 
+        return True, z
 
 
 
 def step(P, x):
     import math
     C = []
-    for i in range(0, len(P)): 
+    for i in range(0, len(P)):
         C.append([P[i][-2], P[i][-1]- inner_product(x, P[i][:-2])])
-    lowerbound = - math.inf 
-    upperbound = math.inf 
-    lbIndex = None 
-    ubIndex = None 
-    # calculate lower and upperbound for x 
-    for i in range(len(C)): 
-        # improve lowerbound 
+    lowerbound = - math.inf
+    upperbound = math.inf
+    lbIndex = None
+    ubIndex = None
+    # calculate lower and upperbound for x
+    for i in range(len(C)):
+        # improve lowerbound
         if C[i][0] > 0 and C[i][1] / C[i][0] > lowerbound:
-            lowerbound =   C[i][1] / C[i][0] 
-            lbIndex = i 
-        # correct upperbound 
-        if C[i][0] < 0 and C[i][1] / C[i][0] < upperbound: 
+            lowerbound =   C[i][1] / C[i][0]
+            lbIndex = i
+        # correct upperbound
+        if C[i][0] < 0 and C[i][1] / C[i][0] < upperbound:
             upperbound = C[i][1] / C[i][0]
-            ubIndex = i 
+            ubIndex = i
     print(lowerbound, upperbound)
     assert upperbound >= lowerbound or abs(lowerbound -upperbound) < 10**(-12)
     w= [lowerbound if lowerbound != -math.inf else upperbound ]
@@ -364,7 +335,42 @@ def step(P, x):
     return x+w
 
 
-def test3(P, z): 
+
+# TEST INSTANCES
+
+def test():
+    file = open("project_instances.dat", "r")
+    setup = []
+    instance = None # {"k": , "data": ""}
+    content = ""
+    for line in file:
+        if len(line) == 0:
+            continue
+        if line[0] == "k":
+            if instance != None:
+                instance["data"] = content
+                setup.append(instance)
+                content = ""
+            kValue = int(line.strip()[-1])
+            instance = {"k": kValue, "data": None}
+        else:
+            content += line
+
+    k = []
+    for i in range(len(setup)):
+        f = open("test/test{}.dat".format(i),"w")
+        content = setup[i]["data"]
+        f.write(content)
+        f.close()
+
+        k.append(setup[i]["k"])
+
+    for i in range(len(k)):
+        projection("test/test{}.dat".format(i), k[i], "test/solution.dat")
+
+    print("done")
+
+def test3(P, z):
     import numpy as np
     P = np.array(P)
     z = np.array(z)
@@ -381,19 +387,3 @@ def test4(P, z):
     A = P[:,:-1]
     b = P[:,-1]
     assert np.count_nonzero(A.dot(z) >= b) == A.shape[0]
-
-
-
-
-        
-
-
-
-
- 
-
-
-
-
-
-
---
+++ adm-i/program-exercise-01/fourier-motzkin.sh
@@ -0,0 +1,66 @@
+#!/bin/bash#
+echo ''
+echo '_        __        ______  _____          _______    _____    ____
+(__    __) \    ___)    /  \    |        |      /   |    / _  \
+   |  |     |  (__     /    \   |  |\/|  |     /    |_  (_/ )  )
+   |  |     |   __)   /  ()  \  |  |  |  |    (__    _)    /  /
+   |  |     |  (___  |   __   | |  |  |  |      _|  |_    /  /__
+___|  |____/       )_|  (__)  |_|  |__|  |_____(      )__(      )_'
+echo ""
+echo ""
+echo "~ˁ˚ᴥ˚ˀ~"
+echo "Hello Sven"
+echo "How are you?"
+echo ""
+echo "---------------------"
+echo ""
+
+if [ "$1" == "project" ]; then
+  # echo "Run project"
+  python3 project.py $2 $3 $4
+elif [ "$1" == "image" ]; then
+  #echo "Run image"
+  python3 image.py $2 $3 $4
+elif [ "$1" == "H_representation" ]; then
+  #echo "Run H_representation"
+  python3 H_representation.py $2 $3
+elif [ "$1" == "compute_x_or_y" ]; then
+  #echo "Run compute_x_or_y"
+  python3 compute_x_or_y.py $2
+else
+  echo 'Unknown parameter given. Try
+./fourier-motzkin.sh project inputfile k outputfile
+./fourier-motzkin.sh image polyhedron matrix outputfile
+./fourier-motzkin.sh H_representation inputfile outputfile
+./fourier-motzkin.sh compute_x_or_y inputfile'
+  echo ""
+  echo ""
+fi
+
+echo ""
+echo "https://www.instagram.com/official_team42/"
+echo ""
+echo ""
+
+echo "Wish you all the best"
+echo '     / \
+    / _ \
+   | / \ |
+   ||   || _______
+   ||   || |\     \
+   ||   || ||\     \
+   ||   || || \    |
+   ||   || ||  \__/
+   ||   || ||   ||
+    \\_/ \_/ \_//
+   /   _     _   \
+  /               \
+  |    O     O    |
+  |   \  ___  /   |
+ /     \ \_/ /     \
+/  -----  |  -----  \
+|     \__/|\__/     |
+\       |_|_|       /
+ \_____       _____/
+       \     /
+       |     |'
